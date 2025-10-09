a700d5f8d7bc2f7e29a78ef3bbeb4e1a2fca5f87
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2025-10-09 20:38:39 +0900

    Update vila_setup.sh

--- vila_setup.sh
+++ vila_setup.sh
@@ -3,13 +3,13 @@
 # This is required to activate conda environment
 eval "$(conda shell.bash hook)"
 
-CONDA_ENV=${1:-""}
-if [ -n "$CONDA_ENV" ]; then
-    conda create -n $CONDA_ENV python=3.10 -y
-    conda activate $CONDA_ENV
-else
-    echo "Skipping conda environment creation. Make sure you have the correct environment activated."
-fi
+# CONDA_ENV=${1:-""}
+# if [ -n "$CONDA_ENV" ]; then
+#     conda create -n $CONDA_ENV python=3.10 -y
+#     conda activate $CONDA_ENV
+# else
+#     echo "Skipping conda environment creation. Make sure you have the correct environment activated."
+# fi
 
 # This is required to enable PEP 660 support
 python -m pip install --upgrade pip setuptools
